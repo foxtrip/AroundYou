55fc3f271fa5dbed17df25575105828d8d29c12d
foxtrip <ambrosia012@gmail.com>
2016-10-14 14:03:32 +0900

    react-router-s

---
+++ client/src/components/UploadPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UploadPage extends React.Component{
+  constructor(props){
+    super(props);
+  };
+  render(){
+    return (
+      <div>
+        <h1>UploadPage</h1>
+        <i> Welcome!</i>
+      </div>
+    );
+  };
+};
+
+export default UploadPage;
