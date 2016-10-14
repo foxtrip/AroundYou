719e0ee725b90d542c3007330462949ccea2ea0f
foxtrip <ambrosia012@gmail.com>
2016-10-14 13:50:04 +0900

    ajax

--- server/main.js
+++ server/main.js
@@ -7,7 +7,6 @@ import Pin from '../build/gmapsModel.js';//call mongoose Model
 import http from 'http';
 
 const app = express();
-import bodyParser from 'body-parser';
 
 app.set("port", 7777);
 
@@ -21,10 +20,6 @@ mongoose.connect('mongodb://localhost/localDB');
 
 app.use(bodyParser.urlencoded({ extended: true }));//?
 app.use(bodyParser.json());//?
-<<<<<<< 5715ac5d064adbed8891f9cb29301df6703fee9c
-=======
-//초기화면
->>>>>>> ajax11
 app.use(express.static(__dirname + './../client/public'));  //server 에 정적파일 띄우기
 app.get('/data', (req,res) => {	//mongoDB pin data 모두 보냄(array)/ok
 	Pin.find((err,pins)=>{
@@ -44,38 +39,18 @@ app.use('*', (req, res)=>{  //react-router 사용위한 tool
 //     tag:req.body.tag,
 //     image:req.body.image
 //   });
-<<<<<<< 73a92a2c4479620cea1813a3f9df3e4e1f25dbee
-<<<<<<< 5715ac5d064adbed8891f9cb29301df6703fee9c
+
 //   pin.save(function(err,pin){  //pin객체 저장
-=======
-//   pin.save(function(err,data){  //pin객체 저장
->>>>>>> ajax11
-=======
-//   pin.save(function(err,pin){  //pin객체 저장
->>>>>>> google login
 //     if(err) { console.error(err) };
 //     res.send('success');
 //   });
 // });
-<<<<<<< 5715ac5d064adbed8891f9cb29301df6703fee9c
-=======
-
->>>>>>> ajax11
 const demoPin = new Pin({ //demo 객체 생성
 	  userid: "demo",
 	  lat:37.583248,
 	  lng:126.985183,
 	  tag: "midnight",
 	  image:""
-});
-const demoPin2 = new Pin({ //demo2 객체 생성
-	  userid: "demo2",
-	  lat:37.582547,
-	  lng:126.983531,
-	  tag: "북촌 11길",
-	  image:""
-});
-
 });
 const demoPin2 = new Pin({ //demo2 객체 생성
 	  userid: "demo2",
@@ -97,9 +72,4 @@ demoPin2.save((err, demoPin)=> { //mongodb에 저장
 
 const server = app.listen(app.get("port"), () => {  
   console.log("Express listening on port", app.get("port"));
-<<<<<<< 5715ac5d064adbed8891f9cb29301df6703fee9c
-=======
-
->>>>>>> ajax11
 });
-
