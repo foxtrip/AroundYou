a1f636109f63a1296bd26002baf7d03910cb1f9d
foxtrip <ambrosia012@gmail.com>
2016-10-14 14:03:32 +0900

    react-router-s

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout'
 import App from './components/App';
-import Upload from './components/Upload'
-import Login from './components/Login'
+import UploadPage from './components/UploadPage'//변경
+import LoginPage from './components/LoginPage'//변경
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 const rootElement = document.getElementById('wrap');
 ReactDOM.render((<Router history={browserHistory}>
 					<Route path='/' component={Layout}>
 						<IndexRoute component={App}/>
-						<Route path="upload" component={Upload}/>
-						<Route path="login" component={Login}/>
+						<Route path="upload" component={UploadPage}/>
+						<Route path="login" component={LoginPage}/>
 						{/*<Route path="*" component={NoMatch}/>*/}
 					</Route>
 				</Router>
