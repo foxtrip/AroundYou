b21d006b0d8647c638ab5f307663dcfbb0d6a696
foxtrip <ambrosia012@gmail.com>
2016-10-15 13:41:20 +0900

    google login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,11 +14,11 @@ class Login extends React.Component{
 
   responseGoogle(googleUser) {  //1. 구글 로그인 클릭하면 token 받음 
     // // Useful data for your client-side scripts:
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+    // const profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail());
     // // The ID token you need to pass to your backend:
     const id_token = googleUser.getAuthResponse().id_token;//standard JWT         
 
