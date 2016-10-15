3272e9cd0a493feb6003a7f2658c86c730d9a078
foxtrip <ambrosia012@gmail.com>
2016-10-15 14:04:13 +0900

    login fin

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -13,13 +13,14 @@ class Login extends React.Component{
   };
 
   responseGoogle(googleUser) {  //1. 구글 로그인 클릭하면 token 받음 
-    // // Useful data for your client-side scripts:
-    // const profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail());
-    // // The ID token you need to pass to your backend:
+    console.log(googleUser);
+    // Useful data for your client-side scripts:
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    // The ID token you need to pass to your backend:
     const id_token = googleUser.getAuthResponse().id_token;//standard JWT         
 
     $.ajax({  //ajax 이용해 서버에 token 보내거나, 구글에 보낸다.
